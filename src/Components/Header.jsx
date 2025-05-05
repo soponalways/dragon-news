050b9cdd8d40445293e99b22f99ad88ea8df13bb
soponalways <soponalways@gmail.com>
2025-05-05 08:06:21 +0600

    theme coustomization and font and date fns integration

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from '../assets/logo.png'; 
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div>
-            Hello from headers
+        <div className='flex flex-col justify-center items-center gap-4'>
+            <img className='w-[400px]' src={logo} alt="" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-accent font-semibold'>{format(new Date(), `EEEE, MMMM dd, yyyy`) }</p>
         </div>
     );
 };
