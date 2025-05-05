d68a79167a8d07be408bbaa9af9acf2a20cbcdd0
soponalways <soponalways@gmail.com>
2025-05-05 13:56:31 +0600

    Load news Data and Filtered news Data Based on Category

--- src/Components/Homelayout/RightAside.jsx
+++ src/Components/Homelayout/RightAside.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
-import FindUsOn from './FindUsOn';
 
 const RightAside = () => {
     return (
--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} = useParams(); 
+    const data = useLoaderData(); 
+
+    const [categoryNews , setCategoryNews] = useState([]); 
+    console.log(categoryNews)
+
+    // Handle Side Effect 
+    useEffect(() => {
+        if (id === '0') {
+            setCategoryNews(data);
+            return;
+        } else if (id === '1') {
+            const breakingNews = data.filter(news => news.others.is_today_pick === true); 
+            setCategoryNews(breakingNews); 
+            return; 
+        } else {
+            const filteredData = data.filter(news => news.category_id == id); 
+            // console.log(filteredData)
+            setCategoryNews(filteredData); 
+        }; 
+
+    }, [data, id]); 
+
+
     return (
         <div>
-            <h1 className="text-3xl font-semibold text-center">Category News - {id}</h1>
+            <h1 className="text-3xl font-semibold text-center">Total - {categoryNews.length} News Found</h1>
         </div>
     );
 };
